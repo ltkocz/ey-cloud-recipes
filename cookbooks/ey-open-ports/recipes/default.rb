11b1b8ed9f006f6a53d2b1e834b527867195d149
Laurent TKOCZ <ltkocz@MacBook-Pro-de-Laurent.local>
2013-07-22 23:33:33 +0200

    managing rights adding by security group

--- cookbooks/ey-open-ports/recipes/default.rb
+++ cookbooks/ey-open-ports/recipes/default.rb
@@ -1,9 +1,10 @@
-# port numbers that you wish to open up on your environment 
+# port numbers that you wish to open up on your environment
+front_env = "front"
 ports = [
   {:port => 21, :protocol => 'tcp', :ip_range =>'0.0.0.0/0'},
-  {:port =>3306, :protocol => 'tcp', :ip_range =>'172.31.38.164/32'},
-  {:port =>9312, :protocol => 'tcp', :ip_range => "172.31.38.164/32"},
-  {:port =>11300, :protocol => 'tcp', :ip_range =>"172.31.38.164/32"}
+  {:port =>3306, :protocol => 'tcp', :ip_range =>'front'},
+  {:port =>9312, :protocol => 'tcp', :ip_range => "front"},
+  {:port =>11300, :protocol => 'tcp', :ip_range =>"front"}
 ]
 
 chef_gem "aws-sdk" do
@@ -19,7 +20,7 @@ ruby_block "open up ports via EC2 security groups" do
     # find security group for environment
     env_name = node['engineyard']['environment']['name']
     security_groups = ec2.security_groups.filter("group-name","*#{env_name}*")
-    
+    front_group = ec2.security_groups.filter("group-name","*#{front_env}*").first
     security_groups.each do |security_group|
       open_ports = security_group.ingress_ip_permissions.select{|ip_permission| 
         ip_permission.groups.empty?
@@ -30,8 +31,9 @@ ruby_block "open up ports via EC2 security groups" do
         if open_ports.include?("#{port[:port]}:#{port[:protocol]}")
           Chef::Log.info "Port #{port[:port]}:#{port[:protocol]} is already open (open ports: #{open_ports.join(', ')})"
         else
-          Chef::Log.info "Opening port #{port[:port]}:#{port[:protocol]} to #{port[:ip_range]}"
-          security_group.authorize_ingress(port[:protocol].to_sym, port[:port],port[:ip_range])
+          target = port[:ip_range] == "front" ? front_group : port[:ip_range]
+          Chef::Log.info "Opening port #{port[:port]}:#{port[:protocol]} to #{target.class.to_s == 'String' ? target : target.name}"
+          security_group.authorize_ingress(port[:protocol].to_sym, port[:port],target)
         end
       end
     end
