6e4ccdb5b1ada2f3e68fbca53ad416877829a752
Laurent TKOCZ <ltkocz@MacBook-Pro-de-Laurent.local>
2013-07-28 20:44:28 +0200

    added beanstalked url env var

--- cookbooks/env_vars/attributes/env_vars.rb
+++ cookbooks/env_vars/attributes/env_vars.rb
@@ -3,9 +3,5 @@
 # The example below will tune garbage collection for REE and Ruby 1.9.x and higher 
  
 default[:env_vars] = {
-  :RUBY_HEAP_MIN_SLOT => "10000",
-  :RUBY_HEAP_SLOTS_INCREMENT => "10000",
-  :RUBY_HEAP_SLOTS_GROWTH_FACTOR => "1.8",
-  :RUBY_GC_MALLOC_LIMIT => "8000000",
-  :RUBY_HEAP_FREE_MIN => "4096",
+  :BEANSTALK_URL => "beanstalk://172.31.33.216:11300",
 }
